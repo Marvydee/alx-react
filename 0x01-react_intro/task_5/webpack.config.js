430ea787a22ecb8278807b5bb1664bb478a7615b
Marvydee <marvydee1@gmail.com>
2024-08-25 21:17:42 +0100

    Create a project using Webpack

--- 0x01-react_intro/task_5/webpack.config.js
+++ 0x01-react_intro/task_5/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: {
     main: path.resolve(__dirname, "./src/index.js"),
   },
-
+  devtool: "inline-source-map",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
